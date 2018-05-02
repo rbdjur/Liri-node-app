88724904c2d8686511a0ef1bbed17b4757894fe4
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-05-01 18:04:36 -0700

    added functions that define how to call APIs for twitter, spotify, and omdb to work successfully. Next objective, create console.log that easily display the relevant information

--- liri.js
+++ liri.js
@@ -1,18 +1,90 @@
 console.log("Hello");
 // put require method into a variable to be easily called. 
 require("dotenv").config();
-//Takes in all command line arguments
-var inputString = process.argv;
-// console.log("This is the dotEnv variable", dotEnv);
 
-var keysFile = require("./keys.js");
-console.log("This is keys info", keysFile);
+// require keys.js and store it into keysFile
+var keys = require("./keys.js");
+// require twitter and store into variable twitter
+var Twitter = require('twitter');
+// require request and store in variable request
+var request = require("request");
+// require spotify and store in variable Sptoify 
+var Spotify = require('node-spotify-api');
+// require fs
+var fs = require("fs");
+// // movieKey
+// var movieKey = parseInt(f496ae5a);
 
 // create variables to hold the API keys of Twitter and Spotify
-// var spotify = new Spotify(keys.Spotify);
-// console.log(spotify);
-var Twitter = require('twitter');
-var client = new Twitter(keysFile.Twitter);
+var spotify = new Spotify(keys.Spotify);
+var client = new Twitter(keys.Twitter);
+
+//Takes in all command line arguments
+var inputString = process.argv[2];
+var value = process.argv.slice(3);
+
+switch (inputString) {
+    case "my-tweets":
+        apiTwitter();
+        break;
+
+    case "spotify-this-song":
+        if (process.argv.length < 4) {
+            value = "Mine"
+        }
+        apiSpotify(value);
+        break;
+    
+    case "movies-this":
+    if (process.argv.length < 4) {
+        value = "Training Day"
+    }
+    getMovie();
+    break;
+}
+
+// function calling apiTwitter
+function apiTwitter() {
+    var params = { screen_name: 'artcronaldo', count: 21 };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log("@artcronaldo:" + tweets[i].text);
+                fs.appendFile("log.txt", "@artcronaldo:" + tweets[i].text);
+            }
+        }
+    });
+}
+
+// function calling Spotify + running the variable value through the function
+function apiSpotify(value) {
+    spotify.search({ type: 'track', query: value }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data);
+        console.log("Future - Incredible", data.tracks.items[0]);
+        // console log so the data can be pretty and easily  read (artist, sont title, year, duration);
+
+
+        // console.log(JSON.stringify(data.tracks.items[0]))
+        // var song = data.tracks.items[0];
+        
+    });
+}
+
+function getMovie() {
+    request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body);
+        } else {
+            console.log("getMovie error: ", error);
+        }
+    });
+}
+
+
+
 
 
 
@@ -44,11 +116,6 @@ var client = new Twitter(keysFile.Twitter);
 //         console.log(dataArr);
 // });
 
-// // create variables to hold the API keys of Twitter and Spotify
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
-// console.log(spotify);
-// console.log(client);
 
 
 
